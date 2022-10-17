c6f05530a5ed5e1faae71da56831f9019a95a53e
mick84 <demenkoff1984@yahoo.com>
2022-10-18 01:11:50 +0300

    make timer after

--- public/scripts/js/app.js
+++ public/scripts/js/app.js
@@ -2,17 +2,45 @@
 const form = document.getElementById("control-panel");
 const board = document.getElementById("board");
 const cells = [...board.children];
+const announce = document.getElementById("announce");
+const playerTables = document.querySelectorAll(".player-table");
 function makeMark(cell, mark) {
     cell.classList.add(mark, "marked");
 }
 function clearBoard() {
     cells.forEach((el) => el.classList.remove("marked", "x", "o"));
 }
-form.onsubmit = function (e) {
+async function delay(seconds) {
+    return new Promise((resolve) => setTimeout(() => resolve(), seconds * 1000));
+}
+async function showCountdown(el, steps) {
+    for (let i = 0; i <= steps; i++) {
+        await delay(1);
+        el.textContent = `${steps - i}`;
+    }
+}
+showCountdown(playerTables[1].querySelector(".timer"), 5);
+form.onreset = function (e) {
+    e.preventDefault();
+    //state.continue = false;
+    state.players.forEach((p) => (p.score = 0));
+    playerTables.forEach((t, i) => (t.querySelector(".scores").textContent = `${state.players[i].score}`));
+};
+form.onsubmit = async function (e) {
     e.preventDefault();
     const dataArr = new FormData(form);
     const data = Object.fromEntries(dataArr);
+    const { beginnerSymbol } = data;
+    const otherSymbol = beginnerSymbol === "x" ? "o" : "x";
+    state.continue = true;
+    state.currentPlayerIndex = 0;
+    state.players[0].symbol = `${beginnerSymbol}`;
+    state.players[1].symbol = otherSymbol;
     state.boardSize = +`${data["board-size"]}`;
+    state.strickLength = +`${data["strick-length"]}`;
+    state.maxTimePerTurn = +`${data.countdown}`;
+    state.boardMap = Array.from({ length: state.boardSize }, () => new Array(state.boardSize).fill(""));
+    announce.innerText = "";
     board.style.setProperty("--board-size", `${state.boardSize}`);
     switch (state.boardSize) {
         case 3:
@@ -29,30 +57,109 @@ form.onsubmit = function (e) {
             cells.forEach((el) => el.removeAttribute("hidden"));
             break;
     }
-    state.boardSize === 4 && clearBoard();
-};
-board.onclick = function (event) {
-    const cell = event.target;
-    if (cell === this || cell.classList.contains("marked")) {
-        return;
-    }
-    makeMark(cell, "o");
-    console.log(cell);
+    clearBoard();
+    playerTables.forEach((table, i) => {
+        table.querySelector(".scores").textContent = `${state.players[i].score}`;
+        table.querySelector(".symbol").textContent = `${state.players[i].symbol}`;
+    });
 };
 const state = {
     boardSize: 3,
+    strickLength: 3,
     continue: false,
     currentPlayerIndex: 0,
-    players: [],
+    maxTimePerTurn: 0,
+    boardMap: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ],
+    players: [
+        { score: 0, symbol: "x" },
+        { score: 0, symbol: "o" },
+    ],
+};
+board.onclick = async function (event) {
+    const cell = event.target;
+    if (cell === this || cell.classList.contains("marked") || !state.continue) {
+        return;
+    }
+    const { symbol } = state.players[state.currentPlayerIndex];
+    makeMark(cell, symbol);
+    const number = +cell.dataset.number;
+    const [row, col] = [
+        Math.floor(number / state.boardSize),
+        number % state.boardSize,
+    ];
+    state.boardMap[row][col] = symbol;
+    //check  row:
+    const victorySeq = symbol.repeat(state.strickLength);
+    if (state.boardMap[row].join("").includes(victorySeq)) {
+        console.log("row!");
+        state.continue = false;
+        state.players[state.currentPlayerIndex].score++;
+        announce.innerText = `player ${state.currentPlayerIndex + 1} won!`;
+        return;
+    }
+    //check  column:
+    const column = state.boardMap.map((row) => row[col]);
+    if (column.join("").includes(victorySeq)) {
+        console.log("column!");
+        state.continue = false;
+        state.players[state.currentPlayerIndex].score++;
+        announce.innerText = `player ${state.currentPlayerIndex + 1} won!`;
+        return;
+    }
+    //check  first-diagonal:\
+    const firstDiag = [[row, col]];
+    for (let i = 1, j = 1; row - i >= 0 && col - j >= 0; i++, j++) {
+        firstDiag.push([row - i, col - j]);
+    }
+    for (let i = 1, j = 1; row + i < state.boardSize && col + j < state.boardSize; i++, j++) {
+        firstDiag.push([row + i, col + j]);
+    }
+    const firstDiagSeq = firstDiag
+        .map((el) => state.boardMap[el[0]][el[1]])
+        .join("");
+    if (firstDiagSeq.includes(victorySeq)) {
+        console.log("1diag!");
+        state.continue = false;
+        state.players[state.currentPlayerIndex].score++;
+        announce.innerText = `player ${state.currentPlayerIndex + 1} won!`;
+        return;
+    }
+    //check second diagonal:/
+    const secondDiag = [[row, col]];
+    for (let i = 1, j = 1; row + i < state.boardSize && col - j >= 0; i++, j++) {
+        secondDiag.push([row + i, col - j]);
+    }
+    for (let i = 1, j = 1; row - i >= 0 && col + j < state.boardSize; i++, j++) {
+        secondDiag.push([row - i, col + j]);
+    }
+    const secondDiagSeq = secondDiag
+        .map((el) => state.boardMap[el[0]][el[1]])
+        .join("");
+    if (secondDiagSeq.includes(victorySeq)) {
+        console.log("2diag!");
+        state.continue = false;
+        state.players[state.currentPlayerIndex].score++;
+        announce.innerText = `player ${state.currentPlayerIndex + 1} won!`;
+        return;
+    }
+    //check if there are free cells on board:
+    state.continue = !!cells.filter((cell) => !cell.hasAttribute("hidden") && !cell.classList.contains("marked")).length;
+    if (!state.continue) {
+        //draw case:
+        announce.innerText = `TIE!`;
+    }
+    //if not won, next turn:
+    state.currentPlayerIndex = state.currentPlayerIndex === 0 ? 1 : 0;
+    //check if strick of given length is reached:
 };
 const turn = (state) => { };
 const init = (state) => {
     //clearBoard();
     //show menu, ask for board size and for beginning sign
-    state.continue = true;
     //get beginning sign from form and put it to player_0
-    state.players = [
-        { score: 0, symbol: "x" },
-        { score: 0, symbol: "o" },
-    ];
 };
+//init(state);
